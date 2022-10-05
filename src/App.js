1861284744f624a79d7c47aa735c66bdc946011c
Tea Abuselidze <teona.abus@gmail.com>
2022-10-05 15:42:13 -0500

    V3

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import './App.css';
-import React from 'react'
-import Navbar from './components/Navbar';
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Work from './pages/Work'
-import {Route, Routes} from "react-router-dom"
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Work from "./pages/Work";
+import { Route, Routes } from "react-router-dom";
 
 var colors = ["#554348", "#514355", "#474355", "#434a55"];
 var currentIndex = 0;
 
-setInterval(function() {
-	document.body.style.cssText = "background-color: " + colors[currentIndex];
-	currentIndex++;
-	if (currentIndex === undefined || currentIndex >= colors.length) {
-		currentIndex = 0;
-	}
+setInterval(function () {
+  document.body.style.cssText = "background-color: " + colors[currentIndex];
+  currentIndex++;
+  if (currentIndex === undefined || currentIndex >= colors.length) {
+    currentIndex = 0;
+  }
 }, 1000);
 
 function App() {
-  return(
-<>
-    <Navbar/>
-    <div>
-      <Routes> 
-        <Route path="/work" element={<Work/>} />
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
-      </Routes>
-    </div>
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/work" element={<Work />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,62 +1,58 @@
-import React from 'react'
-import {Link, useMatch, useResolvedPath} from "react-router-dom"
-import {RightOutlined} from '@ant-design/icons';
-import './Home.css'
-export default function Home(){
-    return (
-        <>
-        <div class="container">
-        <div class="name">
-        TEA ABUSELIDZE
-      </div>
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { RightOutlined } from "@ant-design/icons";
+import "./Home.css";
 
-      <div class="description">
-        <p class="typing-demo">
-        Software Engineer and Front End Developer
-        </p>
-      </div>
+export default function Home() {
+  return (
+    <>
+          <div class="container">
+        <div class="name">TEA ABUSELIDZE</div>
 
-      <div class="container1">
-      <CustomLink to="/work" style={{ textDecoration: 'none', color: '#FFFFFA'}}>
-        <div class="projects">
-        <div>
-        <RightOutlined style={{ marginTop: '1px', color: '#FFFFFA'}}/>
+        <div class="description">
+          <p class="typing-demo">Software Engineer and Front End Developer</p>
         </div>
-        <div>
-          see my projects
-        </div>
-        </div>
-        </CustomLink>
-        
 
-        <CustomLink to="/about" style={{ textDecoration: 'none', color: '#FFFFFA'}}>
-        <div class="learn">
-          <div>
-          <RightOutlined style={{ marginTop: '1px', color: '#FFFFFA'}}/>
-          </div>
-        <div>
-          learn more
-        </div>
-        </div>
-        </CustomLink>
-      </div>
-      </div>
+        <div class="container1">
+          <CustomLink
+            to="/work"
+            style={{ textDecoration: "none", color: "#FFFFFA" }}
+          >
+            <div class="projects">
+              <div>
+                <RightOutlined style={{ marginTop: "1px", color: "#FFFFFA" }} />
+              </div>
+              <div class="hover-underline-animation">see my projects</div>
+            </div>
+          </CustomLink>
 
-      </>
-    )
-    
+          <CustomLink
+            to="/about"
+            style={{ textDecoration: "none", color: "#FFFFFA" }}
+          >
+            <div class="learn">
+              <div>
+                <RightOutlined style={{ marginTop: "1px", color: "#FFFFFA" }} />
+              </div>
+              <div class="hover-underline-animation">learn more</div>
+            </div>
+          </CustomLink>
+        </div>
+      </div>
+    </>
+  );
 }
 
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
-function CustomLink({to, children, ...props}) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({path: resolvedPath.pathname, end: true})
-  
-      return(
-          <div className={isActive ? "active" : ""}>
-              <Link to={to} {...props}>
-                  {children}
-              </Link>
-          </div>
-      )
-  }
+  return (
+    <div className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </div>
+  );
+}
+
